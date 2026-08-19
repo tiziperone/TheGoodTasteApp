01d1c17cf08a97195e1928c35fd9b44438505318
tiziperone <tiziperonetobias@gmail.com>
2026-08-19 07:44:01 -0300

    Se realizó la pagina de productos

--- The Good Taste/Entidades/Producto.cs
+++ The Good Taste/Entidades/Producto.cs
@@ -8,13 +8,12 @@ namespace The_Good_Taste.Entidades
         public string Codigo { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
-        public decimal Precio { get; set; }
+        public decimal Precio { get; set; } // Propiedad unificada
         public int Stock { get; set; }
         public int StockMinimo { get; set; }
         public string UrlImagen { get; set; }
         public int IdCategoria { get; set; }
-        public Categoria Categoria { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime? DeletedAt { get; set; } // Para el soft-delete (historial eliminados)
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime? DeletedAt { get; set; }
     }
 }
