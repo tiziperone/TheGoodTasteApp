7848c763f3a8db7247bf9c4f408c9452cda8acc5
tiziperone <tiziperonetobias@gmail.com>
2026-08-26 10:55:16 -0300

    Se cambio el nombre de la conexion a bd para que no busque y no salte el error

--- The Good Taste/Datos/Conexion.cs
+++ The Good Taste/Datos/Conexion.cs
@@ -5,7 +5,7 @@ namespace The_Good_Taste.Datos
     internal static class Conexion
     {
         // Si tu instancia de SQL Server es SQLEXPRESS, cambia "Data Source=." por "Data Source=.\\SQLEXPRESS"
-        private static readonly string CadenaConexion = "Data Source=.;Initial Catalog=TheGoodTasteDB;Integrated Security=True;TrustServerCertificate=True;";
+        private static readonly string CadenaConexion = "SQLEXPRESS=.;Initial Catalog=TheGoodTasteDB;Integrated Security=True;TrustServerCertificate=True;";
 
         internal static SqlConnection ObtenerConexion()
         {
