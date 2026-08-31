123a2feda0718a83307eccd3c65eda2ed7134869
tiziperone <tiziperonetobias@gmail.com>
2026-08-31 10:34:14 -0300

    Se realizó el diseño del formulario de Usuarios en base al formulario fijado por la cátedra

---
+++ TheGoodTaste.UI/FormUsuarios.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace TheGoodTaste.UI
+{
+    partial class FormUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.textBoxApellido = new System.Windows.Forms.TextBox();
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.textBoxDNI = new System.Windows.Forms.TextBox();
+            this.textBoxDir = new System.Windows.Forms.TextBox();
+            this.dateTimePickerFechNac = new System.Windows.Forms.DateTimePicker();
+            this.textBoxNroTel = new System.Windows.Forms.TextBox();
+            this.radioButtonHom = new System.Windows.Forms.RadioButton();
+            this.radioButtonMuj = new System.Windows.Forms.RadioButton();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonDel = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.radioButtonAct = new System.Windows.Forms.RadioButton();
+            this.radioButtonInac = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(31, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Apellido:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Usuario:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Contraseña:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 212);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Perfil";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(386, 38);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 16);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "DNI:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(386, 74);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 16);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Dirección";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(386, 109);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(116, 16);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Fecha Nacimiento";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(386, 141);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(61, 16);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Teléfono";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(386, 174);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(38, 16);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "Sexo";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(125, 35);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(181, 22);
+            this.textBoxName.TabIndex = 11;
+            // 
+            // textBoxApellido
+            // 
+            this.textBoxApellido.Location = new System.Drawing.Point(125, 74);
+            this.textBoxApellido.Name = "textBoxApellido";
+            this.textBoxApellido.Size = new System.Drawing.Size(181, 22);
+            this.textBoxApellido.TabIndex = 12;
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.Location = new System.Drawing.Point(125, 109);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(181, 22);
+            this.textBoxUser.TabIndex = 13;
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(125, 141);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(181, 22);
+            this.textBoxPass.TabIndex = 14;
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Location = new System.Drawing.Point(125, 174);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(181, 22);
+            this.textBoxEmail.TabIndex = 15;
+            // 
+            // textBoxDNI
+            // 
+            this.textBoxDNI.Location = new System.Drawing.Point(510, 36);
+            this.textBoxDNI.Name = "textBoxDNI";
+            this.textBoxDNI.Size = new System.Drawing.Size(153, 22);
+            this.textBoxDNI.TabIndex = 17;
+            // 
+            // textBoxDir
+            // 
+            this.textBoxDir.Location = new System.Drawing.Point(510, 71);
+            this.textBoxDir.Name = "textBoxDir";
+            this.textBoxDir.Size = new System.Drawing.Size(153, 22);
+            this.textBoxDir.TabIndex = 18;
+            // 
+            // dateTimePickerFechNac
+            // 
+            this.dateTimePickerFechNac.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePickerFechNac.Location = new System.Drawing.Point(510, 103);
+            this.dateTimePickerFechNac.Name = "dateTimePickerFechNac";
+            this.dateTimePickerFechNac.Size = new System.Drawing.Size(153, 22);
+            this.dateTimePickerFechNac.TabIndex = 19;
+            // 
+            // textBoxNroTel
+            // 
+            this.textBoxNroTel.Location = new System.Drawing.Point(510, 138);
+            this.textBoxNroTel.Name = "textBoxNroTel";
+            this.textBoxNroTel.Size = new System.Drawing.Size(153, 22);
+            this.textBoxNroTel.TabIndex = 20;
+            // 
+            // radioButtonHom
+            // 
+            this.radioButtonHom.AutoSize = true;
+            this.radioButtonHom.Location = new System.Drawing.Point(510, 170);
+            this.radioButtonHom.Name = "radioButtonHom";
+            this.radioButtonHom.Size = new System.Drawing.Size(77, 20);
+            this.radioButtonHom.TabIndex = 21;
+            this.radioButtonHom.TabStop = true;
+            this.radioButtonHom.Text = "Hombre";
+            this.radioButtonHom.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonMuj
+            // 
+            this.radioButtonMuj.AutoSize = true;
+            this.radioButtonMuj.Location = new System.Drawing.Point(593, 170);
+            this.radioButtonMuj.Name = "radioButtonMuj";
+            this.radioButtonMuj.Size = new System.Drawing.Size(61, 20);
+            this.radioButtonMuj.TabIndex = 22;
+            this.radioButtonMuj.TabStop = true;
+            this.radioButtonMuj.Text = "Mujer";
+            this.radioButtonMuj.UseVisualStyleBackColor = true;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(750, 35);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(136, 74);
+            this.buttonSave.TabIndex = 23;
+            this.buttonSave.Text = "Guardar";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
+            // buttonDel
+            // 
+            this.buttonDel.Location = new System.Drawing.Point(750, 119);
+            this.buttonDel.Name = "buttonDel";
+            this.buttonDel.Size = new System.Drawing.Size(136, 77);
+            this.buttonDel.TabIndex = 24;
+            this.buttonDel.Text = "Cancelar";
+            this.buttonDel.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Administrador",
+            "Cliente"});
+            this.comboBox1.Location = new System.Drawing.Point(125, 209);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(181, 24);
+            this.comboBox1.TabIndex = 25;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(34, 352);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(834, 201);
+            this.dataGridView1.TabIndex = 26;
+            // 
+            // radioButtonAct
+            // 
+            this.radioButtonAct.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButtonAct.AutoSize = true;
+            this.radioButtonAct.Location = new System.Drawing.Point(34, 311);
+            this.radioButtonAct.Name = "radioButtonAct";
+            this.radioButtonAct.Size = new System.Drawing.Size(61, 26);
+            this.radioButtonAct.TabIndex = 27;
+            this.radioButtonAct.TabStop = true;
+            this.radioButtonAct.Text = "Activos";
+            this.radioButtonAct.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonInac
+            // 
+            this.radioButtonInac.Appearance = System.Windows.Forms.Appearance.Button;
+            this.radioButtonInac.AutoSize = true;
+            this.radioButtonInac.Location = new System.Drawing.Point(110, 311);
+            this.radioButtonInac.Name = "radioButtonInac";
+            this.radioButtonInac.Size = new System.Drawing.Size(70, 26);
+            this.radioButtonInac.TabIndex = 28;
+            this.radioButtonInac.TabStop = true;
+            this.radioButtonInac.Text = "Inactivos";
+            this.radioButtonInac.UseVisualStyleBackColor = true;
+            // 
+            // FormUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 590);
+            this.Controls.Add(this.radioButtonInac);
+            this.Controls.Add(this.radioButtonAct);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.buttonDel);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.radioButtonMuj);
+            this.Controls.Add(this.radioButtonHom);
+            this.Controls.Add(this.textBoxNroTel);
+            this.Controls.Add(this.dateTimePickerFechNac);
+            this.Controls.Add(this.textBoxDir);
+            this.Controls.Add(this.textBoxDNI);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxUser);
+            this.Controls.Add(this.textBoxApellido);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormUsuarios";
+            this.Text = "FormUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.TextBox textBoxApellido;
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.TextBox textBoxDNI;
+        private System.Windows.Forms.TextBox textBoxDir;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFechNac;
+        private System.Windows.Forms.TextBox textBoxNroTel;
+        private System.Windows.Forms.RadioButton radioButtonHom;
+        private System.Windows.Forms.RadioButton radioButtonMuj;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.RadioButton radioButtonAct;
+        private System.Windows.Forms.RadioButton radioButtonInac;
+    }
+}
