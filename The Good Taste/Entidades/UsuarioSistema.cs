de7aaeebfaf9f0ad4a7b0fc8c1fd413c01f70584
tiziperone <tiziperonetobias@gmail.com>
2026-09-05 09:59:40 -0300

    Se agregaron los roles o "perfiles" a la entidad usuario

--- The Good Taste/Entidades/UsuarioSistema.cs
+++ The Good Taste/Entidades/UsuarioSistema.cs
@@ -2,13 +2,20 @@
 
 namespace The_Good_Taste.Entidades
 {
+    public enum RolUsuario
+    {
+        Admin = 1,
+        Gerente = 2,
+        Vendedor = 3
+
+    }
     public class UsuarioSistema
     {
         public int IdUsuario { get; set; }
         public string NombreUsuario { get; set; }
         public string Email { get; set; }
         public string PasswordHash { get; set; }
-        public string Rol { get; set; } // "admin" o "vendedor"
+        public RolUsuario Rol { get; set; } // "admin" o "vendedor"
         public bool Activo { get; set; }
         public DateTime? LastSeenAt { get; set; }
     }
