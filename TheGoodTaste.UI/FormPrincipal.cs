bd1f5dbfaf735e6f24b9bd29621caecd547e717a
tiziperone <tiziperonetobias@gmail.com>
2026-09-17 15:54:43 -0300

    Se hizo uso de la propiedad "Enable = false" para que los botones a los otros formularios se "oscurezcan" dependiendo del perfil con el que se inicio seison

--- TheGoodTaste.UI/FormPrincipal.cs
+++ TheGoodTaste.UI/FormPrincipal.cs
@@ -41,15 +41,18 @@ namespace TheGoodTaste.UI
             switch (_usuarioActual.Rol)
             {
                 case RolUsuario.Admin:
+                    // El Admin tiene acceso total, todos los botones quedan habilitados por defecto
                     break;
 
                 case RolUsuario.Gerente:
-                    if (btnUsuarios != null) btnUsuarios.Visible = false;
+                    // Apagamos Usuarios
+                    if (btnUsuarios != null) btnUsuarios.Enabled = false;
                     break;
 
                 case RolUsuario.Vendedor:
-                    if (btnUsuarios != null) btnUsuarios.Visible = false;
-                    if (btnProductos != null) btnProductos.Visible = false;
+                    // Apagamos Usuarios y Productos
+                    if (btnUsuarios != null) btnUsuarios.Enabled = false;
+                    if (btnProductos != null) btnProductos.Enabled = false;
                     break;
             }
         }
@@ -60,19 +63,20 @@ namespace TheGoodTaste.UI
             switch (keyData)
             {
                 case Keys.F1:
-                    if (btnProductos != null && btnProductos.Visible) btnProductos.PerformClick();
+                    // Verificamos .Enabled en lugar de .Visible para bloquear el atajo si no hay permisos
+                    if (btnProductos != null && btnProductos.Enabled) btnProductos.PerformClick();
                     return true;
                 case Keys.F2:
-                    if (btnClientes != null && btnClientes.Visible) btnClientes.PerformClick();
+                    if (btnClientes != null && btnClientes.Enabled) btnClientes.PerformClick();
                     return true;
                 case Keys.F3:
-                    if (btnVentas != null && btnVentas.Visible) btnVentas.PerformClick();
+                    if (btnVentas != null && btnVentas.Enabled) btnVentas.PerformClick();
                     return true;
                 case Keys.F4:
-                    if (btnUsuarios != null && btnUsuarios.Visible) btnUsuarios.PerformClick();
+                    if (btnUsuarios != null && btnUsuarios.Enabled) btnUsuarios.PerformClick();
                     return true;
                 case Keys.Escape:
-                    if (btnSalir != null) btnSalir.PerformClick();
+                    if (btnSalir != null && btnSalir.Enabled) btnSalir.PerformClick();
                     return true;
             }
             return base.ProcessCmdKey(ref msg, keyData);
